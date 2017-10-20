b7b1da6a16b3a7a564b90455a780282af4486152
LAPTOP-VOD70US0\antoi <akeribin@gmail.com>
2017-10-20 16:57:41 +0200

    Finalisation du design et code de connexion

--- js/monCode.js
+++ js/monCode.js
@@ -1,26 +1,23 @@
-// A $( document ).ready() block.
+var username = "admin";
+var password = "UHS";
 
 $(document).ready(function () {
-    console.log("ready!");
-
-
-<<<<<<< HEAD
-    $(".connect_button").click(function (){
-     $(this).animate({
-            left: '250px',
-            opacity: '0.5',
-            height: '150px',
-            width: '150px'
-=======
-    $(".connect_input").click(function () {
-        if ($(this).prev('input').attr('type') == 'password');
-            changeType($(this).prev('input'), 'text');
-        else
-            changeType($(this).prev('input'), 'password');
-
-            return false;
->>>>>>> a698b2844a3f470b26549b52ffd0e60345c4c333
-        });
+    $('.connect_inner_button').click(function(){
+        console.log('click');
+        verifyInputs();
     });
-
 });
+
+
+function verifyInputs(){
+    console.log('verify');
+    if(username == $('#username').val() && password == $('#password').val()){
+        connectToUHS();
+    }
+}
+
+function connectToUHS(){
+    window.location ='C:/Users/antoi/Documents/HistoireSimple/UHS-WEB/home.html';
+}
+
+//faire une fonction qui déclenche le bouton quand on clique sur entrée
