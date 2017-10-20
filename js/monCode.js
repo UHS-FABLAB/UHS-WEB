fbc0fb08541f791e183c14a129cfd31bbbc7860a
chloeazd <chloe.deazevodomendez@ynov.com>
2017-10-20 15:14:04 +0200

    no message

--- js/monCode.js
+++ js/monCode.js
@@ -4,18 +4,13 @@ $(document).ready(function () {
     console.log("ready!");
 
 
-    $(".connect_input").click(function () {
-        if ($(this).prev('input').attr('type') == 'password')
-            changeType($(this).prev('input'), 'text');
-        else
-            changeType($(this).prev('input'), 'password');
-
-            return false;
+    $(".connect_button").click(function (){
+     $(this).animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '150px',
+            width: '150px'
         });
-    }
-
-    $('.connect_button').click(function () {
-        alert('vous êtes connectez');
     });
 
 });
