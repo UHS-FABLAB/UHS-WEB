76541485b3a92c7debb6507fd255ea9d2efb5028
Unknown <antoinekeribin@hotmail.fr>
2017-12-21 12:11:52 +0100

    Merge inchala

--- js/js_media.js
+++ js/js_media.js
@@ -1,8 +1,33 @@
 $(document).ready(function () {
+    //gère les onglets Ajout/modif
     $('.onglet').click(function(){
         $('.onglet').removeClass('onglet_actif');
         if(!$(this).hasClass('onglet_actif')){
             $(this).addClass('onglet_actif');
         }
+        if($(this).hasClass('onglet_0')){
+            $('.contenu_onglet_add').addClass('contenu_onglet_actif');
+            $('.contenu_onglet_modif').removeClass('contenu_onglet_actif');
+        }else{
+            $('.contenu_onglet_modif').addClass('contenu_onglet_actif');
+            $('.contenu_onglet_add').removeClass('contenu_onglet_actif');   
+        }
     });
+    //gère les onglets video/jeuvideo
+    $('.film_add_video_onglet, .film_add_jeuvideo_onglet').click(function(){
+        $('.film_add_video_onglet, .film_add_jeuvideo_onglet').removeClass('film_add_onglet_actif');
+        if(!$(this).hasClass('film_add_onglet_actif')){
+            $(this).addClass('film_add_onglet_actif');
+        }
+        if($(this).hasClass('film_add_video_onglet')){
+            //afficher contenu video
+            $('.film_add_video').addClass('film_add_elem_active');
+            $('.film_add_jeuvideo').removeClass('film_add_elem_active');
+        }else{
+            //afficher contenu jeuvideo
+            $('.film_add_video').removeClass('film_add_elem_active');
+            $('.film_add_jeuvideo').addClass('film_add_elem_active');
+        }
+    });
+
 });
